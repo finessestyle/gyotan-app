f99002f02827d9bffc57490208de81cf2693fd36
finessestyle <finessestyle1@gmail.com>
2024-03-27 16:31:23 +0900

    20240327

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,20 +1,41 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import {
+  View, Text, TextInput, TouchableOpacity, StyleSheet
+} from 'react-native'
 
 import Button from '../../components/Button'
 import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 const handlePress = (): void => {
   // 会員登録
-  router.push('/memo/list')
+  router.replace('/memo/list')
 }
 
 const SighUp = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
-        <TextInput style={styles.input} value='Email Address' />
-        <TextInput style={styles.input} value='Password' />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => { setEmail(text) }}
+          autoCapitalize='none'
+          keyboardType='email-address'
+          placeholder='Email Address'
+          textContentType='emailAddress'
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => { setPassword(text) }}
+          autoCapitalize='none'
+          secureTextEntry
+          placeholder='Password'
+          textContentType='password'
+        />
         <Button label='Submit' onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,16 +1,23 @@
 import { View, StyleSheet } from 'react-native'
-
-import { router } from 'expo-router'
+import { router, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import LogOutButton from '../../components/LogOutButton'
 
 const handlePress = (): void => {
   router.push('/memo/create')
 }
 
-const List = (): JSX.Element => {
+const Index = (): JSX.Element => {
+  const navigation = useNavigation()
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => { return <LogOutButton /> }
+    })
+  }, [])
   return (
     <View style={styles.container}>
       <View>
@@ -32,4 +39,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default List
+export default Index
